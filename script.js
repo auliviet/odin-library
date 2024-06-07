3de3e3fa6d7276e44a5ae51cec8b015861bc0039
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-06-07 20:22:46 +1000

    Create function to display the list of books in the table

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
+const myLibrary = [];
+
+// Book object
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        if (this.read == true) {
-            return `${this.title} by ${this.author}, ${this.pages} pages, already read`
-        }
-        else {
-            return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`
-        } 
+}
+
+
+// Update the read status of a book.
+Book.prototype.updateRead = function () {
+    if (this.read == true) {
+        this.read = false;
+    }
+
+    else {
+        this.read = true;
     }
 }
+
+
+// Get content from the form and create a new book object in the myLibrary array.
+function addBookToLibrary(newBook) {
+
+
+    myLibrary.push(newBook);
+}
+
+function displayBooks() {
+// Display each book in myLibrary on the page.
+
+    let table = document.querySelector("tbody");
+
+    for (book of myLibrary) {
+        let row = document.createElement("tr");
+
+        for (key in book) {
+
+            // Go through each book attribute excluding the methods defined in the prototype
+            if (book.hasOwnProperty(key)) {
+
+                // Fill each cell in the table with the content of the object
+                let cell = document.createElement("td");
+                cell.textContent = book[key];
+                row.appendChild(cell);
+            }
+
+            
+        }
+
+        // Add the row of values to the table
+        table.appendChild(row);
+    }
+}
+
+// For testing
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+addBookToLibrary(theHobbit);
+const otherBook = new Book("Other book", "Fake author", 200, false);
+addBookToLibrary(otherBook);
+
+displayBooks();
